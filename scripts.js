9dd33cd0083e390c4534b991e6300a167df49149
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-03-03 18:57:22 +0100

    creacion de fila oculta (dummy)

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const urlAPI = "http://localhost:3001/facturas";
-let facturas;
+const filadummy = document.querySelector(".dummy");
+filadummy.cloneNode(true);
 
+let facturas;
 (async () => {
   const respuesta = await fetch(urlAPI);
   facturas = await respuesta.json();
 }
 )();
+
+filadummy.textContent = ("");
+filadummy.classList.add("ocultado");
