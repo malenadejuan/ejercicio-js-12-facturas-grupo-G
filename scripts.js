9db23e577d0818f1a3139109c1993e1273fa5b1c
Malena de Juan <malena.dj4@gmail.com>
2021-03-03 16:50:07 +0100

    api llamada y guardos los datos en variable facturas

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const urlAPI = "http://localhost:3001/facturas";
+let facturas;
+
+(async () => {
+  const respuesta = await fetch(urlAPI);
+  facturas = await respuesta.json();
+  console.log(facturas);
+}
+)();
