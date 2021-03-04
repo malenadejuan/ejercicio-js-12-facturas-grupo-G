ea4b8ee7ac3b0d84149cd0434189e4d66f3c7c4c
Malena de Juan <malena.dj4@gmail.com>
2021-03-04 20:12:18 +0100

    celdas fecha con la fecha formateada

--- scripts.js
+++ scripts.js
@@ -6,14 +6,16 @@ const facturas = fetch(urlAPI).then(respuesta => respuesta.json()).then(dato => 
 
 async function nuevaFila() {
   let nuevaFila;
+  let fechaVencimiento;
   let fecha;
   const tabla = document.querySelector(".facturas");
   for (const factura of await facturas) {
     if (factura.tipo === "ingreso") {
       nuevaFila = filadummy.cloneNode(true);
-      fecha = luxon.DateTime.fromMillis(Number(factura.vencimiento));
+      fechaVencimiento = luxon.DateTime.fromMillis(Number(factura.vencimiento));
+      fecha = luxon.DateTime.fromMillis(Number(factura.fecha));
       nuevaFila.querySelector(".numero").textContent = factura.numero;
-      nuevaFila.querySelector(".fecha").textContent = factura.fecha;
+      nuevaFila.querySelector(".fecha").textContent = fecha.toLocaleString();
       nuevaFila.querySelector(".concepto").textContent = factura.concepto;
       nuevaFila.querySelector(".base").textContent = factura.base;
       const precioConIVA = `${Math.round((factura.tipoIva * factura.base) / 100)} € (21%)`;
@@ -23,7 +25,7 @@ async function nuevaFila() {
       nuevaFila.querySelector(".estado").textContent = factura.abonada;
       if (factura.abonada === false) {
         nuevaFila.querySelector(".estado").classList.add("noAbonada");
-        nuevaFila.querySelector(".vence").textContent = fecha.toLocaleString();
+        nuevaFila.querySelector(".vence").textContent = fechaVencimiento.toLocaleString();
       } else {
         nuevaFila.querySelector(".vence").textContent = "-";
       }
